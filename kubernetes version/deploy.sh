ed407b9530eb292e7bb87ac01d33b11d786fdfa2
Behnam Ebrahimi <behnam.ebrahimi@gmail.com>
2020-01-03 17:08:12 +1100

    added k8s version

--- kubernetes version/deploy.sh
+++ kubernetes version/deploy.sh
@@ -1,17 +1,17 @@
-docker image build -t behnamebrahimi/docker-complex-client -t behnamebrahimi/docker-complex-client:$MIN_SHA -f ./client/Dockerfile.dev ./client
-docker image build -t behnamebrahimi/docker-complex-server -t behnamebrahimi/docker-complex-server:$MIN_SHA -f ./server/Dockerfile.dev ./server
-docker image build -t behnamebrahimi/docker-complex-worker -t behnamebrahimi/docker-complex-worker:$MIN_SHA -f ./worker/Dockerfile.dev ./worker
+docker build -t behnamebrahimi/docker-complex-client:latest -t behnamebrahimi/docker-complex-client:$GIT_SHA -f ./client/Dockerfile.dev ./client
+docker build -t behnamebrahimi/docker-complex-server:latest -t behnamebrahimi/docker-complex-server:$GIT_SHA -f ./server/Dockerfile.dev ./server
+docker build -t behnamebrahimi/docker-complex-worker:latest -t behnamebrahimi/docker-complex-worker:$GIT_SHA -f ./worker/Dockerfile.dev ./worker
 
 docker push behnamebrahimi/docker-complex-client:latest
 docker push behnamebrahimi/docker-complex-server:latest
 docker push behnamebrahimi/docker-complex-worker:latest
 
-docker push behnamebrahimi/docker-complex-client:$MIN_SHA
-docker push behnamebrahimi/docker-complex-server:$MIN_SHA
-docker push behnamebrahimi/docker-complex-worker:$MIN_SHA
+docker push behnamebrahimi/docker-complex-client:$GIT_SHA
+docker push behnamebrahimi/docker-complex-server:$GIT_SHA
+docker push behnamebrahimi/docker-complex-worker:$GIT_SHA
 
 kubectl apply -f k8s/
 
-kubectl set image deployments/server-deployment server=behnamebrahimi/docker-complex-server:$MIN_SHA
-kubectl set image deployments/client-deployment client=behnamebrahimi/docker-complex-client:$MIN_SHA
-kubectl set image deployments/worker-deployment worker=behnamebrahimi/docker-complex-worker:$MIN_SHA
+kubectl set image deployments/server-deployment server=behnamebrahimi/docker-complex-server:$GIT_SHA
+kubectl set image deployments/client-deployment client=behnamebrahimi/docker-complex-client:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=behnamebrahimi/docker-complex-worker:$GIT_SHA
